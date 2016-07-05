7d2c57020fbf23e2bb7cfbd607c09abd1af1273c
Colin Osborn <colin.osborn@gmail.com>
2016-07-05 10:04:56 -0600

    fun fun fun

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
   console.log($('input[type=checkbox]'));
+
+  $('.repbulican').toggleClass('red');
+  $('.democrat').toggleClass('blue');
+  $('.term').toggleClass('yellow');
+  $('.whig').css({
+    backgroundColor: 'purple',
+    fontColor: 'white'
+    });
 });
