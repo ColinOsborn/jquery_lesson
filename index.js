c0e5359a8820121de8dc738308958d13829704da
Colin Osborn <colin.osborn@gmail.com>
2016-07-05 11:35:09 -0600

    events

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 $(document).ready(function(){
   console.log($('input[type=checkbox]'));
 
-  $('.republican').toggleClass('red');
-  $('.democrat').toggleClass('blue');
-  $('.term').toggleClass('yellow');
-  $('.whig').css({
-    backgroundColor: 'purple',
-    color: 'white'
-    });
+  // $('.republican').toggleClass('red');
+  // $('.democrat').toggleClass('blue');
+  // $('.term').toggleClass('yellow');
+  // $('.whig').css({
+  //   backgroundColor: 'purple',
+  //   color: 'white'
+  //   });
+  //
+  // $('.died').next().addClass('green');
+  // $('.one-term').addClass('red');
+  // $('input:checkbox:checked').parent().addClass('blue');
+  // $('input:not(:checked)').parent().siblings().addClass('yellow')
+  // $(':not(:checked)').parent().siblings().addClass('yellow')
+  // $('.died .term').append('<span class="died">(Died)<span>');
+  // $('.died').children('.term').append('<span class="died">(Died)<span>');
+  // $('.number').prepend('<input type="radio">');
 
-  $('.died').next().addClass('green');
-  $('.one-term').addClass('red');
-  $('input:checked').parent('td').addClass('blue');
+  $('checkbox').on('click', function(){
+    $(this).parent().addClass("blue");
+  });
+  
 });
 
 //
